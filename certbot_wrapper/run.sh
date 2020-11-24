b1184c265ae7094d2e22acd45d63962df3d16ca3
loovjo <jonathan.loov@gmail.com>
2020-11-24 23:03:03 +0100

    Properly generate certificates

--- certbot_wrapper/run.sh
+++ certbot_wrapper/run.sh
@@ -1,5 +1,19 @@
+cpath='/etc/letsencrypt/live/coral.shoes'
+
+# generate fake cert
 mkdir -p /etc/letsencrypt/live/coral.shoes/
 openssl req -x509 -nodes -newkey rsa:4096 -days 1\
-    -keyout '/etc/letsencrypt/live/coral.shoes/privkey.pem'\
-    -out '/etc/letsencrypt/live/coral.shoes/fullchain.pem'\
+    -keyout "$cpath/privkey.pem"\
+    -out "$cpath/fullchain.pem"\
     -subj '/CN=localhost'
+
+# wait for nginx to load the cert
+sleep 2
+
+# generate real cert
+certbot certonly --webroot -w /var/www/certbot \
+    --email 'jonathan.loov@gmail.com' \
+    --rsa-key-size 4096 \
+    --agree-tos \
+    --force-renewal \
+    -d 'coral.shoes'
