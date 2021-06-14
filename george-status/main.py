dc7125a0d35b2ded946fdd550d0e34ac29491b8d
loovjo <jonathan.loov@gmail.com>
2021-06-15 00:05:25 +0200

    coprocessing

--- george-status/main.py
+++ george-status/main.py
@@ -1,26 +1,28 @@
+import time
 import asyncio
 import aiohttp
 
 import george_status
 import analysis
 
+async def query_page(sess, stati, user):
+    start = time.time()
+    try:
+        stati[user.name] = await george_status.george_status(sess, user.link)
+        print(f"{user.name} = success in {time.time() - start:.3}s!")
+    except:
+        print(user.name, "=epic fail")
+
 async def main():
     async with aiohttp.ClientSession() as sess:
         users = await george_status.read_users(sess)
 
         stati = {}
-        for user in users:
-            print("querying", user.name, "...", end="", flush=True)
-            try:
-                stati[user.name] = await george_status.george_status(sess, user.link)
-                print("success!")
-            except:
-                print("epic fail")
 
-        print(stati)
+        print("querying...")
+        await asyncio.gather(*[query_page(sess, stati, user) for user in users])
 
         state = analysis.GeorgeState.from_data(users, stati)
-        print(state.proper_order, state.links)
 
         anal = state.analyze()
         print(anal)
