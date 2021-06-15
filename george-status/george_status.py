b0e867359901c627bce5c841f991ebd68ab3fccc
loovjo <jonathan.loov@gmail.com>
2021-06-15 21:50:38 +0200

    check for FLOC

--- george-status/george_status.py
+++ george-status/george_status.py
@@ -9,19 +9,21 @@ class GeorgeError(BaseException, Enum):
     UNICODE_ERROR = 1
 
 class GeorgeStatus:
-    def __init__(self, user, curr_link, prev_link, next_link):
+    def __init__(self, user, curr_link, prev_link, next_link, disabled_floc):
         self.user = user
         self.curr_link = curr_link
         self.prev_link = prev_link
         self.next_link = next_link
+        self.disabled_floc = disabled_floc
 
     def __str__(self):
-        return f"GeorgeStatus(user={self.user!r}, curr_link={self.curr_link!r}, prev_link={self.prev_link!r}, next_link={self.next_link!r})"
+        return f"GeorgeStatus(user={self.user!r}, curr_link={self.curr_link!r}, prev_link={self.prev_link!r}, next_link={self.next_link!r}, disabled_floc={self.disabled_floc})"
 
 class GeorgeIFrameExtractor(HTMLParser):
     def __init__(self, *args, **kwargs):
         super(GeorgeIFrameExtractor, self).__init__(*args, **kwargs)
         self.frame_url = None
+        self.floc_meta = False
         # TODO: Track iframes without id="george" which point to george.gh0.pw for diagnostics
 
     def handle_starttag(self, tag, attrs):
@@ -32,11 +34,18 @@ class GeorgeIFrameExtractor(HTMLParser):
                 if srcs[0].split("?")[0] == "https://george.gh0.pw/embed.cgi":
                     self.frame_url = srcs[0]
 
+        if tag.lower() == "meta":
+            http_equivs = [val.strip().lower() for key, val in attrs if key.strip() == "http-equiv"]
+            contents_equivs = [val.strip().lower() for key, val in attrs if key.strip() == "content"]
+
+            if http_equivs == ["permissions-policy"] and contents_equivs == ["interest-cohort=()"]:
+                self.floc_meta = True
+
 class GeorgeIFrameAnalyzer(HTMLParser):
     def __init__(self, user, curr_link, *args, **kwargs):
         super(GeorgeIFrameAnalyzer, self).__init__(*args, **kwargs)
 
-        self.george_status = GeorgeStatus(user=user, curr_link=curr_link, prev_link=None, next_link=None)
+        self.george_status = GeorgeStatus(user=user, curr_link=curr_link, prev_link=None, next_link=None, disabled_floc=False)
 
         self.last_link = None
 
@@ -85,6 +94,13 @@ async def george_status(sess, url):
         raise GeorgeError.HTTP_ERROR
     except UnicodeDecodeError as _:
         raise GeorgeError.UNICODE_ERROR
+
+    if "Permissions-Policy" in site_req.headers and \
+        site_req.headers["Permissions-Policy"] == "interest-cohort=()":
+        analyzer.george_status.disabled_floc = True
+
+    if extractor.floc_meta:
+        analyzer.george_status.disabled_floc = True
 
     return analyzer.george_status
 
