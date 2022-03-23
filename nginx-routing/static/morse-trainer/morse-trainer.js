d4a445192bc6ec74a64f22c6ae3263938ec49fb1
loovjo <jonathan.loov@gmail.com>
2022-03-24 00:34:19 +0100

    Speed change

--- nginx-routing/static/morse-trainer/morse-trainer.js
+++ nginx-routing/static/morse-trainer/morse-trainer.js
@@ -4,11 +4,13 @@ const DAH = "DAH";
 const LETTER_SEPARATOR = "LET";
 const WORD_SEPARATOR = "WORD";
 
-const DIT_DURATION = 0.100; // seconds
-const DAH_DURATION = 3 * DIT_DURATION;
-const SYMBOL_SEP = DIT_DURATION;
-const LETTER_SEP = 3 * DIT_DURATION;
-const WORD_SEP = 7 * DIT_DURATION;
+var DIT_DURATION = 0;
+var DAH_DURATION = 0;
+var SYMBOL_SEP = 0;
+var LETTER_SEP = 0;
+var WORD_SEP = 0;
+
+
 
 const MORSE = [
     ["A", ".-"],
@@ -150,88 +152,109 @@ let typed_text = "Hello world! ";
 
 let force_update = false;
 
-window.onload = () => {
-    let main = document.getElementById("main");
+let main = document.getElementById("main");
 
-    document.body.addEventListener("keydown", e => {
-        if (e.key == " " && !event.repeat) {
-            last_press_start = Date.now();
+let speed_inp = document.getElementById("speed");
 
-            let space = get_space();
-            if (space != null)
-                events.push(space);
+function set_speeds(dit_speed_ms) {
+    DIT_DURATION = dit_speed_ms / 1000;
+    DAH_DURATION = 3 * DIT_DURATION;
+    SYMBOL_SEP = DIT_DURATION;
+    LETTER_SEP = 3 * DIT_DURATION;
+    WORD_SEP = 7 * DIT_DURATION;
 
-            last_press_end = null;
-        }
-        if (e.key == "Backspace") {
-            if (events.length == 0 && typed_text.length != 0) {
-                typed_text = typed_text.slice(0, typed_text.length-2) + " ";
-                force_update = true;
-            } else {
-                events = [];
-                last_press_start = null;
-                last_press_end = null;
-            }
-        }
-    });
+    speed_inp.value = dit_speed_ms;
+    localStorage.setItem("speed", dit_speed_ms);
+}
 
-    document.body.addEventListener("keyup", e => {
-        if (e.key == " ") {
-            last_press_end = Date.now();
+if (localStorage.getItem("speed") != null) {
+    set_speeds(+localStorage.getItem("speed"));
+} else {
+    set_speeds(+speed_inp.value);
+}
 
-            let dot = get_dot();
-            if (dot != null)
-                events.push(dot);
+speed_inp.addEventListener("change", e => {
+    set_speeds(+e.target.value);
+});
 
-            last_press_start = null;
-        }
-    });
-
-    let last_displayed_events = null;
-    function update_display() {
-        let button = document.getElementById("button");
-
-        let current_events = events.slice();
-
-        let dot = get_dot();
-        if (dot != null)
-            current_events.push(dot);
+document.body.addEventListener("keydown", e => {
+    if (e.key == " " && !event.repeat) {
+        last_press_start = Date.now();
 
         let space = get_space();
         if (space != null)
-            current_events.push(space);
+            events.push(space);
 
-        if (!force_update && last_displayed_events != null && current_events.join(" ") == last_displayed_events.join(" "))
-            return;
-        force_update = false;
-
-        last_displayed_events = current_events;
-
-        let content = [];
-        for (var i = 0; i < current_events.length; i++) {
-            let thing = current_events[i];
-            content.push(event_span(thing));
-        }
-        let current_word = get_word(current_events, false);
-        content.push(document.createElement("br"));
-        content.push(document.createTextNode(current_word));
-        button.replaceChildren(...content);
-
-        let displayed_text = typed_text + get_word(current_events, false);
-        document.getElementById("text").replaceChildren(span_with_class(typed_text, "typed"), span_with_class(get_word(current_events, false), "typing"));
-
-        if (events.length == 0) {
-            return;
-        }
-
-        let last = events[events.length-1];
-        if (last == WORD_SEPARATOR) {
-            let word = get_word(events, true);
-            typed_text += word;
-            events = [];
-        }
-
+        last_press_end = null;
     }
-    update_display();
-    setInterval(update_display, 0.010);
+    if (e.key == "Backspace") {
+        if (events.length == 0 && typed_text.length != 0) {
+            typed_text = typed_text.slice(0, typed_text.length-2) + " ";
+            force_update = true;
+        } else {
+            events = [];
+            last_press_start = null;
+            last_press_end = null;
+        }
+    }
+});
+
+document.body.addEventListener("keyup", e => {
+    if (e.key == " ") {
+        last_press_end = Date.now();
+
+        let dot = get_dot();
+        if (dot != null)
+            events.push(dot);
+
+        last_press_start = null;
+    }
+});
+
+let last_displayed_events = null;
+function update_display() {
+    let button = document.getElementById("button");
+
+    let current_events = events.slice();
+
+    let dot = get_dot();
+    if (dot != null)
+        current_events.push(dot);
+
+    let space = get_space();
+    if (space != null)
+        current_events.push(space);
+
+    if (!force_update && last_displayed_events != null && current_events.join(" ") == last_displayed_events.join(" "))
+        return;
+    force_update = false;
+
+    last_displayed_events = current_events;
+
+    let content = [];
+    for (var i = 0; i < current_events.length; i++) {
+        let thing = current_events[i];
+        content.push(event_span(thing));
+    }
+    let current_word = get_word(current_events, false);
+    content.push(document.createElement("br"));
+    content.push(document.createTextNode(current_word));
+    button.replaceChildren(...content);
+
+    let displayed_text = typed_text + get_word(current_events, false);
+    document.getElementById("text").replaceChildren(span_with_class(typed_text, "typed"), span_with_class(get_word(current_events, false), "typing"));
+
+    if (events.length == 0) {
+        return;
+    }
+
+    let last = events[events.length-1];
+    if (last == WORD_SEPARATOR) {
+        let word = get_word(events, true);
+        typed_text += word;
+        events = [];
+    }
+
 }
+update_display();
+setInterval(update_display, 0.010);
