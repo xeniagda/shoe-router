d57a8089a5cb65e3507df418a5dcd7c09162bb2d
loovjo <jonathan.loov@gmail.com>
2022-03-25 00:25:58 +0100

    place selection

--- nginx-routing/static/coral.shoes/morse-trainer/free.js
+++ nginx-routing/static/coral.shoes/morse-trainer/free.js
@@ -1,5 +1,15 @@
 morse = new Morse(100, update_display);
 audio = new MorseAudio();
+select = new SelectionHandler(
+    morse,
+    audio,
+    document.getElementById("typed"),
+    document.getElementById("button-play-outer"),
+    document.getElementById("button-stop-outer"),
+    (tx, st, en) => {},
+    () => { morse.force_update = true; },
+    highlight_letter,
+);
 
 bind_speed_input(morse, document.getElementById("speed"));
 bind_volume_input(audio, document.getElementById("volume"));
@@ -31,3 +41,11 @@ function update_display(typed, typing, event_spans) {
     document.getElementById("typing").innerText = typing;
 }
 
+function highlight_letter(idx) {
+    console.log(idx);
+    let before_highlight = document.createTextNode(morse.typed_text.slice(0, idx));
+    let highlighted = span_with_class(morse.typed_text[idx], "type-highlight");
+    let after_highlight = document.createTextNode(morse.typed_text.slice(idx + 1));
+
+    document.getElementById("typed").replaceChildren(before_highlight, highlighted, after_highlight);
+}
--- nginx-routing/static/coral.shoes/morse-trainer/morse-trainer.js
+++ nginx-routing/static/coral.shoes/morse-trainer/morse-trainer.js
@@ -137,7 +137,7 @@ class Morse {
 
         this.set_speeds(dit_speed_ms);
 
-        self = this;
+        let self = this;
         this.redraw_int = setInterval(() => self.redraw(), 10);
 
         this.last_displayed_events = null;
@@ -247,7 +247,7 @@ class Morse {
         content.push(document.createElement("br"));
         content.push(document.createTextNode(current_word));
 
-        self.draw_callback(this.typed_text, get_word(current_events, false), content);
+        this.draw_callback(this.typed_text, get_word(current_events, false), content);
     }
 }
 
@@ -323,12 +323,16 @@ class MorseAudio {
     }
 
     on() {
+        if (this.is_on)
+            return;
         this.gain.gain.setValueAtTime(0, this.audio_ctx.currentTime);
         this.gain.gain.linearRampToValueAtTime(this.volume, this.audio_ctx.currentTime+RAMP_TIME);
         this.is_on = true;
     }
 
     off() {
+        if (!this.is_on)
+            return;
         this.gain.gain.setValueAtTime(this.volume, this.audio_ctx.currentTime);
         this.gain.gain.linearRampToValueAtTime(0, this.audio_ctx.currentTime+RAMP_TIME);
         this.is_on = false;
@@ -355,4 +359,169 @@ function bind_volume_input(audio, volume_inp) {
     });
 }
 
+class SelectionHandler {
+    // play_start(text, start, end)
+    // on_play_end()
+    // on_play_letter(idx)
+    constructor(morse, audio, text_field, play_button, stop_button, on_play_start, on_play_end, on_play_letter) {
+        this.morse = morse;
+        this.audio = audio;
 
+        this.text_field = text_field;
+        this.play_button = play_button;
+        this.stop_button = stop_button;
+
+        this.on_play_start = on_play_start;
+        this.on_play_end = on_play_end;
+        this.on_play_letter = on_play_letter;
+
+        let self = this;
+
+        this.play_button.addEventListener("click", () => self.play());
+        this.stop_button.addEventListener("click", () => self.stop());
+        setInterval(() => self.check(), 10);
+
+        this.last_selection = this.get_selection();
+
+        this.playing = []; // [["on"|"off", "dit"|"dah"|"symb"|"let"|"word", idx]]
+        this.next_at = Date.now();
+
+        setInterval(() => self.tick(), 10);
+    }
+
+    tick() {
+        if (this.next_at == null || Date.now() > this.next_at) {
+            if (this.playing.length > 0) {
+                let next = this.playing[0];
+                this.playing = this.playing.slice(1);
+
+                if (next[0] == "on")
+                    this.audio.on();
+                else
+                    this.audio.off();
+
+                let duration = null;
+                if (next[1] == "dit")
+                    duration = this.morse.DIT_DURATION;
+                else if (next[1] == "dah")
+                    duration = this.morse.DAH_DURATION;
+                else if (next[1] == "symb")
+                    duration = this.morse.SYMBOL_SEP;
+                else if (next[1] == "let")
+                    duration = this.morse.LETTER_SEP;
+                else if (next[1] == "word")
+                    duration = this.morse.WORD_SEP;
+
+                if (duration == null) {
+                    console.log(`Unknown duration for ${next}`);
+                    return;
+                }
+                this.next_at = Date.now() + duration * 1000;
+                this.on_play_letter(next[2]);
+            } else {
+                if (this.next_at != null) {
+                    this.next_at = null;
+                    this.stop_button.classList.remove("active");
+                    this.audio.off();
+
+                    this.on_play_end();
+                }
+            }
+        }
+    }
+
+    stop() {
+        this.next_at = null;
+        this.playing = [];
+        this.stop_button.classList.remove("active");
+        this.audio.off();
+
+        this.on_play_end();
+    }
+
+    get_selection() {
+        let sel = window.getSelection();
+
+        let selected = true;
+        if ((sel.focusNode == null) || (sel.anchorNode == null)) {
+            selected = false;
+        } else if (!sel.focusNode.parentElement.isEqualNode(this.text_field)) {
+            selected = false;
+        } else if (!sel.anchorNode.parentElement.isEqualNode(this.text_field))
+            selected = false;
+        else if (sel.toString().length == 0) {
+            selected = false;
+        }
+
+        if (!selected)
+            return null;
+
+        let start = Math.min(sel.anchorOffset, sel.focusOffset);
+        let end = Math.max(sel.anchorOffset, sel.focusOffset);
+
+        return {"start": start, "end": end, "text": sel.toString()};
+    }
+
+    check() {
+        let selection = this.get_selection();
+        if (selection == null) {
+            this.play_button.classList.remove("active");
+            if (!this.play_button.contains(window.getSelection().focusNode)) {
+                this.last_selection = null;
+            }
+        } else {
+            this.play_button.classList.add("active");
+            this.last_selection = selection;
+        }
+    }
+
+    play() {
+        if (this.last_selection == null)
+            return;
+
+        // TODO: This should be factored out probably
+        this.playing = [];
+        let last_let = false;
+        for (var i = 0; i < this.last_selection["text"].length; i++) {
+            let char_here = this.last_selection["text"][i];
+            let total_idx = i + this.last_selection["start"];
+
+            if (char_here == " " || char_here == "\n") {
+                if (last_let)
+                    this.playing = this.playing.slice(0, this.playing.length-1);
+                this.playing.push(["off", "word", total_idx]);
+                last_let = false;
+            } else {
+                let code = null;
+                for (let m of MORSE) {
+                    if (m.length == 0)
+                        continue;
+                    if (m[0] == char_here)
+                        code = m[1];
+                }
+                if (code == null) {
+                    console.warn(`unknown char ${char_here}`);
+                    continue;
+                }
+                for (let ch of code) {
+                    if (ch == DIT) {
+                        this.playing.push(["on", "dit", total_idx]);
+                        this.playing.push(["off", "symb", total_idx]);
+                    } else {
+                        this.playing.push(["on", "dah", total_idx]);
+                        this.playing.push(["off", "symb", total_idx]);
+                    }
+                }
+                // Remove last letter symbol separator
+                this.playing = this.playing.slice(0, this.playing.length-1);
+                this.playing.push(["off", "let", total_idx]);
+                last_let = true;
+            }
+        }
+        this.on_play_start(this.last_selection["text"], this.last_selection["start"], this.last_selection["end"]);
+
+        this.last_selection = null;
+
+        this.stop_button.classList.add("active");
+    }
+}
