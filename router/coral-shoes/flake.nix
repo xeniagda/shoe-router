66a4471b33715d02d39266e3b8ebe56969d64cc8
xenia <xenia.agda@gmail.com>
2024-07-16 00:46:44 +0200

    Nix flakes for router/coral.shoes

---
+++ router/coral-shoes/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A bare minimum Rust flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (sys:
+      let pkgs = import nixpkgs {
+            system = sys;
+            overlays = [ (import rust-overlay) ];
+          };
+          rust = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" "rust-analyzer" ];
+          };
+          platform = pkgs.makeRustPlatform {
+            rustc = rust;
+            cargo = rust;
+          };
+      in rec {
+        packages.default = platform.buildRustPackage {
+          name = "my-cool-project";
+          src = ./.;
+          cargoLock = { lockFile = ./Cargo.lock; };
+        };
+        devShells.default = pkgs.mkShell {
+          packages = [ rust ];
+        };
+      }
+    );
+}
